2b6a6fe4862a254194d4a11a762d9519ce5cb2f8
Debajit91 <debajitroy544@gmail.com>
2025-12-22 00:04:57 +0600

    query table created and db initialized

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from 'express';
+import initDB from './config/db';
 
 
 
 const app = express()
+
+// initializing DB
+initDB();
 
 app.use(express.json());
 
@@ -10,5 +14,12 @@ app.get('/', (req:Request, res:Response) =>{
     res.send('Hello This is Vehicle Rental System App!')
 })
 
+app.use((req:Request, res:Response) =>{
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path
+  })
+})
 
 export default app;
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,77 @@
-import { Pool } from "pg"
-import config from "."
+import { Pool } from "pg";
+import config from ".";
 
 // DB
 export const pool = new Pool({
-    connectionString: `${config.connection_str}`
-})
+  connectionString: `${config.connection_str}`,
+});
+
+// init DB
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+
+        name VARCHAR(100) NOT NULL,
+
+        email VARCHAR(150) UNIQUE NOT NULL,
+        CHECK (email = LOWER(email)),
+
+        password TEXT NOT NULL,
+        CHECK (LENGTH(password) >=6 ),
+
+        phone TEXT NOT NULL,
+
+        role VARCHAR(20) NOT NULL,
+        CHECK (role IN ('admin', 'customer')),
+
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        )`);
+
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS vehicles(
+            id SERIAL PRIMARY KEY,
+
+            vehicle_name VARCHAR(200) NOT NULL,
+
+            type TEXT NOT NULL,
+
+            registration_number TEXT UNIQUE NOT NULL,
+
+            daily_rent_price NUMERIC NOT NULL,
+            CHECK (daily_rent_price > 0),
+
+            availability_status TEXT NOT NULL,
+            CHECK(availability_status IN ('available', 'booked')),
+
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+            )`);
+
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS bookings(
+            id SERIAL PRIMARY KEY,
+
+            customer_id INT REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+
+            vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE NOT NULL,
+
+            rent_start_date DATE NOT NULL,
+
+            rent_end_date DATE NOT NULL,
+            CHECK (rent_end_date > rent_start_date),
+
+            total_price NUMERIC NOT NULL,
+            CHECK (total_price > 0),
+
+            status TEXT NOT NULL,
+            CHECK (status IN ('active', 'cancelled', 'returned')),
+
+            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+            )
+            `);
+};
+
+export default initDB;
